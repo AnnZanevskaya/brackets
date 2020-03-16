5d078821074953867972d70fe1138b2fff14ac12
AnnZanevskaya <ann.zanevskaya7@gmail.com>
2020-03-16 21:45:46 +0300

    feat: implement brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+ module.exports = function check(str, bracketsConfig) {
+     let brackets = bracketsConfig.map(el => el.join(""));
+
+     for (let i = 0; i < brackets.length; i++) {
+
+         if (str.includes(brackets[i])) {
+             str = str.replace(brackets[i], "");
+             i = -1;
+         }
+     }
+
+     if (str === "") {
+         return true;
+     } else {
+         return false;
+     }
+ }
